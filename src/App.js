402e387d34f84ba30ebe5dbdb9017070c05780b7
ashwiniprkash <100183151+ashwiniprkash@users.noreply.github.com>
2022-07-22 21:43:44 +0530

    Home page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HomePage from './components/HomePage';
+import MainRoutes from './Pages/MainRoutes';
+
+function App() {
+  return (
+    <div className="App">
+    {/* <MainRoutes/> */}
+    <HomePage/>
+    </div>
+  );
+}
+
+export default App;
